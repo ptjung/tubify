7c2e4c0ccb47f1f1d8b9682f9e9326e269800076
Patrick <41390486+PtJung@users.noreply.github.com>
2020-02-09 23:25:20 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
-console.log("<< main.js >>");
-
-let xHttp = new XMLHttpRequest();
-xHttp.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-
-        console.log(this.responseText);
-    }
-};
-xHttp.open("GET", "https://cors-anywhere.herokuapp.com/" + "https://www.google.com/search?q=Evdeki Saat - Ekin", true);
-xHttp.send();
+function resetCache() {
+    // Resets cache: empties forms
+    document.getElementById("inputURL").value = "";
+    document.getElementById("outputURL").value = "";
+}
 
 function copyToClipboard() {
     // Copies the output URL text to clipboard
@@ -26,73 +20,116 @@ function openLinkWindow() {
     }
 }
 
-function accessTrackList(paramURL) {
-    const HTML_SEARCH_LINK = "open.spotify.com/embed";
-
-    // This block represents a Spotify full playlist request
-    let xHttp = new XMLHttpRequest();
-    xHttp.onreadystatechange = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-
-            // Grants access to link with the full playlist, to make a playlist from it
-            let paramContent = this.responseText;
-            let indexInit = paramContent.indexOf(HTML_SEARCH_LINK);
-            let indexFin = paramContent.indexOf("\"", indexInit + 1);
-            makePlaylist(paramContent.substring(indexInit, indexFin));
-        }
-    };
-    xHttp.open("GET", "https://cors-anywhere.herokuapp.com/" + paramURL, true);
-    xHttp.send();
+function setClickable(paramBoolean) {
+    // Function that makes the button elements clickable or not clickable
+    document.getElementById("outputCopy").disabled = paramBoolean;
+    document.getElementById("outputOpen").disabled = paramBoolean;
+    document.getElementById("inputSubmit").disabled = paramBoolean;
 }
 
-function makePlaylist(paramURL) {
-    const MESSAGE_CONVERT = "Converting...";
-    const MESSAGE_FINISH = "Finished!";
+function checkLink(paramString) {
+    // Function that checks if a link is valid to be searched; does not work with checking if the URI works
+    const ACCEPTABLE_SCHEMES = ["", "//", "http://", "https://"];
+    const SUBSECTIONS_LIST = ["playlist/", "album/"];
     const EXPECT_LINK_MATERIAL = "open.spotify.com/";
-    const BASE_COPY_LINK = "https://www.youtube.com/watch_videos?video_ids=";
+    const URI_LENGTH = 22;
 
-    if (paramURL.indexOf(EXPECT_LINK_MATERIAL) >= 0) {
-        console.log("VALID REQUEST");
+    let domainIndex = paramString.indexOf(EXPECT_LINK_MATERIAL);
+    let schemeCheck = false;
 
-        // This block represents a Spotify playlist request
+    if (domainIndex >= 0) {
+        // Check link works by checking necessary URL scheme
+        for (let scheme of ACCEPTABLE_SCHEMES) {
+            schemeCheck = schemeCheck || (paramString.substring(0, domainIndex) == scheme);
+        }
+
+        if (schemeCheck) {
+            // Check link works by finding the existence of the website subsection
+            schemeCheck = false;
+            for (let subsection of SUBSECTIONS_LIST) {
+                schemeCheck = schemeCheck || (paramString.substring(domainIndex + EXPECT_LINK_MATERIAL.length, domainIndex + EXPECT_LINK_MATERIAL.length + subsection.length) == subsection);
+            }
+
+            if (schemeCheck) {
+                // Returns boolean after checking if URI length is met (truncating query)
+                paramString = (paramString.indexOf("?") >= 0) ? paramString.substring(0, paramString.indexOf("?")) : paramString;
+                let slashChar = (paramString.charAt(paramString.length - URI_LENGTH - 1) == "/");
+                return slashChar;
+            }
+            return false;
+        }
+        return false;
+    }
+    return false;
+}
+
+function accessTrackList(paramURL) {
+    const MESSAGE_CONVERT = "Converting...";
+    const MESSAGE_ERROR = "Failed!";
+    const HTML_SEARCH_LINK = "open.spotify.com/embed";
+
+    // Report on the website that the link is loading
+    document.getElementById("reporter").innerHTML = MESSAGE_CONVERT;
+    setClickable(true);
+
+    if (checkLink(paramURL)) {
+        // This block represents a Spotify full playlist request
         let xHttp = new XMLHttpRequest();
         xHttp.onreadystatechange = function() {
             if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
 
-                // Report on the website that the link is loading
-                document.getElementById("reporter").innerHTML = MESSAGE_CONVERT;
-                document.getElementById("outputCopy").disabled = true;
-                document.getElementById("outputOpen").disabled = true;
-
-                // Search and initialize base URL such that video IDs can be appended to it, to form a link
-                let searchLinks = getSearches(this.responseText);
-                if (searchLinks.length > 0) {
-                    document.getElementById("outputURL").value = BASE_COPY_LINK;
-                }
-
-                // Search all the Spotify songs to get their respective YouTube video IDs (if they exist)
-                console.log("POTENTIAL SONG COUNT: " + searchLinks.length);
-                for (let linkIndex = 0; linkIndex < searchLinks.length; linkIndex++) {
-                    setTimeout(function() {
-                        searchSpotify(searchLinks[linkIndex]);
-                    }, 25 * linkIndex);
-                }
-
-                setTimeout(function() {
-                    // Report on the website that the link has finished loading
-                    document.getElementById("reporter").innerHTML = MESSAGE_FINISH;
-                    document.getElementById("outputCopy").disabled = false;
-                    document.getElementById("outputOpen").disabled = false;
-                }, 250 * searchLinks.length);
-
+                // Grants access to link with the full playlist, to make a playlist from it
+                let paramContent = this.responseText;
+                let indexInit = paramContent.indexOf(HTML_SEARCH_LINK);
+                let indexFin = paramContent.indexOf("\"", indexInit + 1);
+                makePlaylist(paramContent.substring(indexInit, indexFin));
             }
         };
         xHttp.open("GET", "https://cors-anywhere.herokuapp.com/" + paramURL, true);
         xHttp.send();
     }
     else {
+        // Invalid request; link material not found
         console.log("INVALID REQUEST");
+        document.getElementById("reporter").innerHTML = MESSAGE_ERROR;
+        setClickable(false);
     }
+}
+
+function makePlaylist(paramURL) {
+    const MESSAGE_FINISH = "Finished!";
+    const BASE_COPY_LINK = "https://www.youtube.com/watch_videos?video_ids=";
+
+    console.log("VALID REQUEST");
+
+    // This block represents a Spotify playlist request
+    let xHttp = new XMLHttpRequest();
+    xHttp.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+
+            // Search and initialize base URL such that video IDs can be appended to it, to form a link
+            let searchLinks = getSearches(this.responseText);
+            if (searchLinks.length > 0) {
+                document.getElementById("outputURL").value = BASE_COPY_LINK;
+            }
+
+            // Search all the Spotify songs to get their respective YouTube video IDs (if they exist)
+            console.log("POTENTIAL SONG COUNT: " + searchLinks.length);
+            for (let linkIndex = 0; linkIndex < searchLinks.length; linkIndex++) {
+                setTimeout(function() {
+                    searchSpotify(searchLinks[linkIndex]);
+                }, 100 * linkIndex);
+            }
+
+            // Report on the website that the link has finished loading
+            setTimeout(function() {
+                document.getElementById("reporter").innerHTML = MESSAGE_FINISH;
+                setClickable(false);
+            }, 350 * searchLinks.length);
+        }
+    };
+    xHttp.open("GET", "https://cors-anywhere.herokuapp.com/" + paramURL, true);
+    xHttp.send();
 }
 
 function searchSpotify(paramSearch) {
@@ -145,21 +182,23 @@ function addSearchVideoID(paramSong) {
             let nameIndexEnd = searchResults.indexOf("\"", nameIndexInit + 1);
             let resName = searchResults.substring(nameIndexInit, nameIndexEnd);
 
-            // Append the ID to the output YouTube link
             if (firstOccurence >= 0) {
+                // Append the ID to the output YouTube link
                 let elemOutURL = document.getElementById("outputURL");
                 if ((elemOutURL.value).indexOf(resName) < 0) {
                     elemOutURL.value += resName + ",";
                 }
                 console.log("APPENDED (name=\"" + paramSong + "\", id=\"" + resName + "\")");
-                console.log("CONTENT AT AREA (len=1000): [" + searchResults.substring(nameIndexInit - HTML_SEARCH_TERM.length, nameIndexInit - HTML_SEARCH_TERM.length + 1000)) + "]";
+                console.log("CONTENT AT AREA (len=250): [" + searchResults.substring(nameIndexInit - HTML_SEARCH_TERM.length, nameIndexInit - HTML_SEARCH_TERM.length + 250)) + "]";
             }
             else {
+                // ERROR 1: Cannot append the ID; no link is found
                 console.log("ERROR 1 (" + paramSong + "): No YouTube link found");
             }
 
         }
         else {
+            // ERROR 2: Cannot request song due to request error
             console.log("ERROR 2 (" + paramSong + "): Song cannot be requested (readyState=\"" + this.readyState + "\", status=\"" + this.status + "\")");
         }
     };
@@ -206,6 +245,14 @@ function getSearches(paramContent) {
             return searches;
         }
     }
-
     return [];
 }
+
+function init() {
+    // Runs on loading main.js
+    console.log("< main.js >");
+    setTimeout(function() {
+        resetCache();
+    }, 1);
+}
+init();
